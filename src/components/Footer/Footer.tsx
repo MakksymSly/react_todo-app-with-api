a5b41ab91dd0c50f1a8dc0d7b44a4ef0e88b8230
Maksym Seliutin <maxwell7k@gmail.com>
2025-01-21 10:29:58 +0200

    fix

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -16,7 +16,7 @@ export const Footer: React.FC<Props> = props => {
     completedTodosLenght,
     handleDeleteAllCompleted,
   } = props;
-
+  const basePath = '/react_todo-app-with-api';
   const filters = Object.values(FilterTodosBy);
 
   return (
@@ -29,7 +29,7 @@ export const Footer: React.FC<Props> = props => {
         {filters.map(filter => (
           <a
             key={filter}
-            href={`./#/${filter !== FilterTodosBy.All && filter.toLowerCase()}`}
+            href={`${basePath}/#/${filter !== FilterTodosBy.All && filter.toLowerCase()}`}
             className={cn('filter__link', {
               selected: filterBy === filter,
             })}
